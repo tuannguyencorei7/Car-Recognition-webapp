763d8a69e96882339f8ce52a8715d60ff42780d0
Nguyễn Trần Hữu Tuấn <tuannguyencorei7@gmail.com>
2021-07-14 08:17:41 +0700

    Update settings.py

--- webapp/settings.py
+++ webapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--*&ay^audwv#^thj-deij-z(t$!hkvunk7c0y2%yhcop!zik8u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
